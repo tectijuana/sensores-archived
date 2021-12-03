9a1e52f2edf5b5a9df5931335add14b83640f865
AmadorFernando <79485246+AmadorFernando@users.noreply.github.com>
2021-12-02 17:39:33 -0800

    Create main.py

---
+++ PYTHON/KY-035 Rotary Encoders/main.py
@@ -0,0 +1,55 @@
+#Amador Garcia Fernando
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+counter = 10
+ 
+Enc_A = 17  
+Enc_B = 27  
+ 
+ 
+def init():
+    print "Rotary Encoder Test Program"
+    GPIO.setwarnings(True)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(Enc_A, GPIO.IN)
+    GPIO.setup(Enc_B, GPIO.IN)
+    GPIO.add_event_detect(Enc_A, GPIO.RISING, callback=rotation_decode, bouncetime=10)
+    return
+ 
+  
+  def rotation_decode(Enc_A):
+    global counter
+    sleep(0.002)
+    Switch_A = GPIO.input(Enc_A)
+    Switch_B = GPIO.input(Enc_B)
+ 
+    if (Switch_A == 1) and (Switch_B == 0):
+        counter += 1
+        print "direction -> ", counter
+        while Switch_B == 0:
+            Switch_B = GPIO.input(Enc_B)
+        while Switch_B == 1:
+            Switch_B = GPIO.input(Enc_B)
+        return
+ 
+    elif (Switch_A == 1) and (Switch_B == 1):
+        counter -= 1
+        print "direction <- ", counter
+        while Switch_A == 1:
+            Switch_A = GPIO.input(Enc_A)
+        return
+    else:
+        return
+ 
+def main():
+    try:
+        init()
+        while True :
+            sleep(1)
+ 
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+ 
+if __name__ == '__main__':
+    main()
