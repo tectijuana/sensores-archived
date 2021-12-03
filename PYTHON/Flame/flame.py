1af7c31d842c5321074a192aa72574e815509656
josue0620 <84551683+josue0620@users.noreply.github.com>
2021-12-02 19:19:32 -0800

    Add files via upload

---
+++ PYTHON/Flame/flame.py
@@ -0,0 +1,16 @@
+#Cuevas Oliva Josue Emanuel 18212164
+
+from machine import Pin
+import utime
+
+flame_sensor = Pin(16, Pin.IN)
+utime.sleep(2)
+
+while True:
+   if flame_sensor.value() == 1:
+       print("Flame Detected")
+       utime.sleep(3)
+   else:
+       print("No Flame")
+       utime.sleep(1)
+utime.sleep(0.1)
