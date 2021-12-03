9bba1eb7b81efd7a633d854c45f7b47a042ebc5c
Luis Alfredo De Jesus Romero <84546168+LuisDeJesusR@users.noreply.github.com>
2021-12-03 00:11:43 -0800

    Create RGB_LED.py

---
+++ PYTHON/RGB LED/RGB_LED.py
@@ -0,0 +1,15 @@
+# De Jesus Romero Luis Alfredo
+
+from machine import Pin
+import time
+
+led_pins = [16,17,18] # Pines en donde esta el cableado del RGB
+leds = [Pin(led_pins[0],Pin.OUT),Pin(led_pins[1],Pin.OUT),
+        Pin(led_pins[2],Pin.OUT)] # matriz de control de pines
+delay_t = 0.1 # segundos de espera entre cambios
+while True: # loop infinito
+    for led in leds: # loop entre cada led
+        led.high() # led encendido
+        time.sleep(delay_t) # espera
+        led.low() # led low
+        time.sleep(delay_t) # espera
