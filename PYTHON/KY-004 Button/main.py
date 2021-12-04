e1c5b8b6e031108056e31ce200549193c193eede
CAMB007 <89505905+CAMB007@users.noreply.github.com>
2021-12-03 17:34:43 -0800

    Create main.py

---
+++ PYTHON/KY-004 Button/main.py
@@ -0,0 +1,12 @@
+#EJERCICIO: Realizar un montaje que sea necesario pulsar 2 Botones para que se encienda el led. - Medina Beltran Carlos Alberto 18212216
+
+from machine import Pin
+import time
+
+button=Pin(2,Pin.IN)
+led=Pin(16,Pin.OUT)
+
+while True:
+    state=button.value()
+    led.value(state)
+    time.sleep(0.5)
