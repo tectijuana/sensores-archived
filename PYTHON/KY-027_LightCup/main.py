db32d5ec8125c63300b1cd4d7a9a3ac416646413
Raúl Alvarez Espinoza <71353362+Rulas-Boy@users.noreply.github.com>
2021-12-03 22:03:43 -0800

    Create main.py

---
+++ PYTHON/KY-027_LightCup/main.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import time
+
+Signal_Pin = Pin(20, Pin.IN)
+LED_Pin = Pin(21, Pin.IRQ_FALLING)
+
+while True:
+    print(Signal_Pin.value())
+    LED_Pin.value(Signal_Pin.value())
+    time.sleep(0.05)
