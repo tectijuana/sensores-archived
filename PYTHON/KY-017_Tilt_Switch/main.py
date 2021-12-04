610a804812c549e02b72d14ffb37ed210fb90a38
CAMB007 <89505905+CAMB007@users.noreply.github.com>
2021-12-03 17:32:10 -0800

    Update main.py

--- PYTHON/KY-017_Tilt_Switch/main.py
+++ PYTHON/KY-017_Tilt_Switch/main.py
@@ -1,21 +1,20 @@
 # Required modules are imported and set up
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+from machine import Pin
 import time
    
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
    
-# Here the input pin is declared, to which the sensor is connected. Additionally the PullUP resistor at the input will be activated
-GPIO_PIN = 24
-GPIO.setup(GPIO_PIN, GPIO.IN)
-   
-print ("Sensor test [press CTRL+C to end test]")
+a = Pin(24, Pin.IN)
+
+# GPIO.setup(GPIO_PIN, GPIO.IN)
    
 # This outputFunction will be executed on signal detection
 def outputFunction(null):
         print("Signal detected")
    
 # When a signal is detected (falling signal edge) the output function is executed
-GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=outputFunction, bouncetime=100) 
+GPIO.add_event_detect(a, GPIO.FALLING, callback=outputFunction, bouncetime=100) 
    
 # main program loop
 try:
