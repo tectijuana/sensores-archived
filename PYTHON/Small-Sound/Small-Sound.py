cf2ee6082cf115b9afe6328e031a604112ebdad7
Ana Karen Martinez <71353396+Karen25-2@users.noreply.github.com>
2021-12-03 23:49:27 -0800

    Create Small-Sound.py

---
+++ PYTHON/Small-Sound/Small-Sound.py
@@ -0,0 +1,21 @@
+# Martinez Estrada Ana Karen
+
+import machine import pin
+import utime import sleep
+
+Sound = Pin (28, Pin.IN)
+led = pin(22,pin.OUT)
+clap_count=1
+
+while True:
+    ded.value(1)
+    if Sound.value()==1 and clao_count==1:
+        led.value(1)
+        Sound.value(0)
+        sleep(0.50)
+        
+    if Sound.value()==1 and clap_count==2:
+        led.value(0)
+        clap_count=1
+        Sound.value(0)
+        sleep(0.5)
