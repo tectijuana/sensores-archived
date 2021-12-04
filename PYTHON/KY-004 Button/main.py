8ca470b7ddb6f6642d72d1fa254494040adec981
CAMB007 <89505905+CAMB007@users.noreply.github.com>
2021-12-03 17:59:36 -0800

    Update main.py

--- PYTHON/KY-004 Button/main.py
+++ PYTHON/KY-004 Button/main.py
@@ -1,4 +1,4 @@
-#EJERCICIO: Realizar un montaje que sea necesario pulsar 2 Botones para que se encienda el led. - Medina Beltran Carlos Alberto 18212216
+#EJERCICIO: Realizar un montaje que sea necesario pulsar 1 boton para que se encienda el led. - Medina Beltran Carlos Alberto 18212216
 
 from machine import Pin
 import time
