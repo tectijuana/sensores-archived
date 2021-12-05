7a22ec519058740467e1b5131da695bd98c52ff1
Adrián Cortez <89440798+AdrianCortez000@users.noreply.github.com>
2021-12-04 20:32:42 -0800

    Update main.py

--- PYTHON/KY-002/main.py
+++ PYTHON/KY-002/main.py
@@ -1,12 +1,8 @@
 # Cortez Flores Adrián Alejandro 18212163
 
+import sys
 import RPi.GPIO as GPIO
 import time
-
-
-KY002 = 7
-LED = 11
-
 
 def toggle_led(null):  # Función para activar LED on/off cuando detecte una señal
     if GPIO.input(LED):
@@ -15,9 +11,14 @@ def toggle_led(null):  # Función para activar LED on/off cuando detecte una se�
         GPIO.output(LED, GPIO.HIGH)  # Si el LED está apagado, se enciende
 
 
+KY002 = 7 # Pin asignado para Sensor Vibración
+LED = 11 # Pin asignado para LED
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(KY002, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED, GPIO.OUT)
+
+
 
 # Cuando una vibración es detectada, la función toggle_led será activada.
 GPIO.add_event_detect(KY002, GPIO.FALLING, callback=toggle_led, bouncetime=100)
