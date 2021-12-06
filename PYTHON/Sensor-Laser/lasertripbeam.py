21ea16b6680e2c5e4db2a3b18b811d3174cae3ae
Raúl Alvarez Espinoza <71353362+Rulas-Boy@users.noreply.github.com>
2021-12-05 18:59:25 -0800

    Update lasertripbeam.py

--- PYTHON/Sensor-Laser/lasertripbeam.py
+++ PYTHON/Sensor-Laser/lasertripbeam.py
@@ -1,42 +1,11 @@
-import RPi.GPIO as GPIO
+#  Verificado y corregido por Alvarez Espinoza Raul - 18212141
+from machine import Pin
 import time
 
-GPIO.setmode(GPIO.BCM)
+laser = Pin(18, Pin.OUT)
 
-# Light sensor pin
-pin = 21
-
-# Laser pin
-ledpin = 20
-
-# Setup pin in and out directions
-GPIO.setup(pin, GPIO.IN)
-GPIO.setup(ledpin, GPIO.OUT)
-
-def laser_detector(pin):
-    #Turn on the laser
-    GPIO.output(ledpin, GPIO.HIGH)
-
-    # Delay for laser to activate
-    time.sleep(0.1)
-
-    # Read the light sensor value and print it to terminal
-    print('Light sensor value: ',GPIO.input(pin))
-
-    # Read light sensor and display status
-    if GPIO.input(pin) == 1:
-        print('Laser detected.')
-    else:
-        print('Laser not detected.')
-
-    # Add time delay
+while True:
+    laser.value(1)
     time.sleep(0.2)
-
-# Start the program loop to activate the laser and read the light sensor.
-try:
-    while True:
-        laser_detector(pin)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+    laser.value(0)
+    time.sleep(0.2)
