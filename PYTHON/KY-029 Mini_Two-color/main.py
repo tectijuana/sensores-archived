818057e29969a280c620cc3ab5e17f898ff85134
Raúl Alvarez Espinoza <71353362+Rulas-Boy@users.noreply.github.com>
2021-12-09 14:29:37 -0800

    Update main.py

--- PYTHON/KY-029 Mini_Two-color/main.py
+++ PYTHON/KY-029 Mini_Two-color/main.py
@@ -1,1 +1,28 @@
+# Codigo hecho y verificado por Alvarez Espinoza Raul - 18212141
 
+from machine import Pin
+import time
+
+'''
+Don't forget the resistors for the leds
+'''
+
+red = Pin(18, Pin.OUT)
+green = Pin(19, Pin.OUT)
+
+while True:
+    red.value(1)
+    green.value(0)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(1)
+    time.sleep(1)
+    
+    red.value(1)
+    green.value(1)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(0)
+    time.sleep(1)
