704e21353449b97de24924ded782dbbaeeace88f
Raúl Alvarez Espinoza <71353362+Rulas-Boy@users.noreply.github.com>
2021-12-09 14:58:07 -0800

    Update main.py

--- PYTHON/KY-009 SMD_RGB/main.py
+++ PYTHON/KY-009 SMD_RGB/main.py
@@ -1,1 +1,53 @@
+# Codigo hecho y verificado por Alvarez Espinoza Raul - 18212141
 
+from machine import Pin
+import time
+
+'''
+Don't forget the resistors for the leds
+'''
+
+red = Pin(19, Pin.OUT)
+green = Pin(18, Pin.OUT)
+blue = Pin(20, Pin.OUT)
+
+while True:
+    red.value(1)
+    green.value(0)
+    blue.value(0)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(1)
+    blue.value(0)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(0)
+    blue.value(1)
+    time.sleep(1)
+    
+    red.value(1)
+    green.value(1)
+    blue.value(0)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(1)
+    blue.value(1)
+    time.sleep(1)
+    
+    red.value(1)
+    green.value(0)
+    blue.value(1)
+    time.sleep(1)
+    
+    red.value(1)
+    green.value(1)
+    blue.value(1)
+    time.sleep(1)
+    
+    red.value(0)
+    green.value(0)
+    blue.value(0)
+    time.sleep(1)
