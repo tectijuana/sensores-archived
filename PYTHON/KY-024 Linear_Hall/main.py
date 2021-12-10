eea301475f0f2b86e1cb5d086af29d2767df0a94
Raúl Alvarez Espinoza <71353362+Rulas-Boy@users.noreply.github.com>
2021-12-09 16:39:19 -0800

    Update main.py

--- PYTHON/KY-024 Linear_Hall/main.py
+++ PYTHON/KY-024 Linear_Hall/main.py
@@ -1,1 +1,11 @@
+# Codigo hecho y verificado por Alvarez Espinoza Raul - 18212141
 
+from machine import Pin, ADC
+import time
+
+digital = Pin(18, Pin.IN)
+analog = ADC(26)
+
+while True:
+    print(str(digital.value()) + ' - ' + str(analog.read_u16()))
+    time.sleep(1)
