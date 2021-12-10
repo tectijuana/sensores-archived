08afecc09657ef3a4da1bc2888386854e45efd52
erik18-s <84939760+erik18-s@users.noreply.github.com>
2021-12-09 16:57:14 -0800

    Update main.py

--- PYTHON/KY-001 18B20 Temp/main.py
+++ PYTHON/KY-001 18B20 Temp/main.py
@@ -1,15 +1,15 @@
 import machine, onewire, ds18x20, time
-
-ds_pin = machine.Pin(16)
+    
+ds_pin = machine.Pin(4)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
-
-roms = ds_sensor.scam()
-print('Temperatura')
-
+    
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+    
 while True:
     ds_sensor.convert_temp()
     time.sleep_ms(750)
     for rom in roms:
-        tnum = round ( ds_sensor.read_temp(rom),2)
-        print(tnum,'Celcius')
-    time.sleep(1)
+      print(rom)
+      print(ds_sensor.read_temp(rom))
+    time.sleep(5)
