d71295c201bb553a64691b8729c6ce2b5a09c210
David Alfredo Avila Jimenez <71294551+alfredo70fil@users.noreply.github.com>
2021-12-10 21:30:47 -0800

    Update main.py

--- PYTHON/KY-013 Analog Temp/main.py
+++ PYTHON/KY-013 Analog Temp/main.py
@@ -1,5 +1,8 @@
-# Codificado por Avila Jimenez David Alfredo
-# Modificado por Alvarez Espinoza Raul
+# Avila Jimenez David Alfredo
+# Se necesita testear el código
+
+# Avila Jimenez David Alfredo
+# Se necesita testear el código
 
 import machine
 from utime import sleep
@@ -9,14 +12,13 @@ def main():
     sensor_temperatura = machine.ADC(26)
     c1 = 0.001129148
     c2 = 0.000234125
-    R1 = 10000
     c3 = 0.0000000876741
+    R1 = 10000.0
 
     while True:
-        voltaje = sensor_temperatura.read_u16()
-        R2 = R1 * (65535.0 / voltaje - 1.0)
+        R2 = R1 / (65535 / float(sensor_temperatura.read_u16()) - 1)
         logR2 = log(R2)
-        temperatura = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2))
+        temperatura = (1/(1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)))-273.0
         print('Temperatura: ' + temperatura + ' °C')
         sleep(2)
 
