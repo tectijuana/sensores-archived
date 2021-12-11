cb94963d520cd38a483af69dd9d72b97b1e04e88
SalasTec13 <89493294+SalasTec13@users.noreply.github.com>
2021-12-10 19:44:02 -0800

    Update codigo.py

--- PYTHON/KY-010/codigo.py
+++ PYTHON/KY-010/codigo.py
@@ -4,15 +4,15 @@
 # La placa que se esta usando es la raspberry pi pico
 # La Libreria RPi No esta en funcionamiento, tienes que cambiarla por la libreria machine
 
-import RPi.GPIO as GPIO
+import RPi.machine as machine
 import time
   
-GPIO.setmode(GPIO.BCM)
+machine.setmode(machine.BCM)
   
 # El pin de entrada que está conectado con el sensor
 
-GPIO_PIN = 21
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+machine_PIN = 21
+machine.setup(machine_PIN, GPIO.IN, pull_up_down = machine.PUD_DOWN)
   
 print "KY-010 Sensor Test [press ctrl+c to end the test]"
   
@@ -21,7 +21,7 @@ def outputFunction(null):
   
 # detección de señal (flanco ascendente).
 
-GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=outputFunction, bouncetime=100) 
+machine.add_event_detect(machine_PIN, GPIO.RISING, callback=outputFunction, bouncetime=100) 
   
 # Bucle del programa principal
 
@@ -32,4 +32,4 @@ try:
 # Trabajo de recolección de residuos una vez finalizado el programa
 
 except KeyboardInterrupt:
-        GPIO.cleanup()
+        machine.cleanup()
