0748646a2fdd3bbcd2a8a252e150372ba70f8e13
René Solis <IoTeacher@users.noreply.github.com>
2021-12-15 15:51:31 -0800

    Create main.py

---
+++ PYTHON/KY-011 Two-color/main.py
@@ -0,0 +1,14 @@
+#Cuevas Oliva Josue Emanuel 18212164
+
+from machine import Pin
+import time
+
+led_pins=[21,20]#Pines de LEDS
+leds=[Pin(led_pins[0],Pin.OUT),Pin(led_pins[1],Pin.OUT)]#Matriz de control de pines
+
+while True:
+    for led in leds:#Iteracion entre cada led
+        led.high()#LED Encendido
+        time.sleep(0.1)#Tiempo de espera
+        led.low()#LED Apagado
+        time.sleep(0.1)#Tiempo de espera
