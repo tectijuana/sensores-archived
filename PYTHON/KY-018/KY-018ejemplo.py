bf7043def1e276ecda345a03a8fde8a826d3e897
Christopher Osuna E <89375469+ChrisOIchi@users.noreply.github.com>
2021-12-14 19:01:17 -0800

    Update KY-018ejemplo.py
    Cambio de programa sin la librería RPi.GPIO que no era compatible con RBP pico

--- PYTHON/KY-018/KY-018ejemplo.py
+++ PYTHON/KY-018/KY-018ejemplo.py
@@ -1,19 +1,14 @@
-import RPi.GPIO as GPIO
+from machine import Pin
 import time
-from gpiozero import MCP3008
-
-led = 7
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led,GPIO.OUT)
-GPIO.output(led,False)
-
-try: 
-	while True:
-		adc = MCP3008(channel=0)
-		voltage = 3.3 * adc.value
-		print("Measured voltage is ",voltage);
-		time.sleep(0.2)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+photo_pin =machine.ADC(26) #GP26 Pin31
+led = Pin(25, Pin.OUT)
+while True:
+    raw = photo_pin.read_u16() #La lectura tal cual lee el RB
+    if (raw>30000): #si la lectura es mayor a 30000 imprimira ON
+        print(str(raw) + " ON")
+        led.on()
+        
+    else: #Si la lectura es menor a 30000 imprimira OFF
+        print(str(raw)+" OFF")
+        led.off()
+    time.sleep(.2)
