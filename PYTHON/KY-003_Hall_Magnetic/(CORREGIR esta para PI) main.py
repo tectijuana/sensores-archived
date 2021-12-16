54b85a62a7537acddef392a96aea9282f3b68c79
Oscar-HM <89495352+Oscar-HM@users.noreply.github.com>
2021-12-15 23:02:24 -0800

    Update (CORREGIR esta para PI) main.py

--- PYTHON/KY-003_Hall_Magnetic/(CORREGIR esta para PI) main.py
+++ PYTHON/KY-003_Hall_Magnetic/(CORREGIR esta para PI) main.py
@@ -1,18 +1,17 @@
 #
 # OSCAR HERNANDEZ MEDINA
 #
-import RPi.GPIO as GPIO
+from machine import Pin
+import utime
 
-GPIO.setmode(GPIO.BCM)
-GPIO_PIR = 24
-print "KY-003 Module Test (CTRL-C = exit)"
+sensor=Pin(26, Pin.IN)
+utime.sleep(1)
 
-GPIO.setup(GPIO_PIR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-def printFunction(channel):
-    print("Detected")
-    GPIO.add_event_detect(GPIO_PIR, GPIO.RISING, callback=printFunction)
-try:
-    while True :
-        Current_State = GPIO.input(GPIO_PIR)
-        except KeyboardInterrupt:
-GPIO.cleanup()
+while True:
+    if sensor.value()==1:
+        print("Campo magnetico detectado")
+        utime.sleep(1)    
+    else:
+        print("Ningun campo detectado")
+        utime.sleep(1)
+utime.sleep(1)
